e54758b8faf43e7e0ef3dfb8ab9ebd67c0deac5d
Peter Francis Robante <robantepeterfran@gmail.com>
2024-04-15 13:57:44 +0800

    feat:evaluation and evaluation question added

---
+++ database/migrations/2024_04_15_031444_create_evaluation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('evaluation', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('evaluated_id')->constrained('users')->where('role', 'Teacher');
+            $table->string('evaluated_first_name');
+            $table->string('evaluated_last_name');
+            $table->string('subject_name');
+            $table->string('semester');
+            $table->string('comment');
+            $table->string('strand');
+            $table->string('year_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evaluation');
+    }
+};
