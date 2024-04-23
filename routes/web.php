3da30d9ab3d18e522a16c437b367a97dfca907b1
Peter Francis Robante <robantepeterfran@gmail.com>
2024-04-23 09:31:09 +0800

    fix:session id added to condition

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication\AuthController;
 
+// Define routes for guest users (unauthenticated)
 Route::middleware(['guest'])->group(function () {
     Route::get('/', function () {
         return view('screen/authentication/login');
-    });
+    })->name('login');
+
     // Login
     Route::post('/login', [AuthController::class, 'adminLogin'])->name('user-login');
+
     // Reset Password
     Route::get('/reset-password', function () {
         // Retrieve the token from the query parameters
@@ -17,15 +20,14 @@ Route::middleware(['guest'])->group(function () {
         return view('screen/authentication/reset')->with('token', $token);
     })->name('reset-password');
     Route::post('/set-new-password', [AuthController::class, 'setNewPassword'])->name('set-new-password');
-
-    // // Redirect to avoid access other routes
-    // Route::redirect('/login', '/');
 });
 
+// Define routes for authenticated users
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', function () {
         return view('screen/dashboard/index');
-    });
+    })->name('home');
+
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
     // Redirect authenticated users away from the login and register endpoints
